02a07c96d0cbbcdde870dd4c0c3f2f0b468ca37d
rahman-wq <rahmancoolboy34@gmail.com>
2024-07-27 22:15:44 +0800

    build feat:sigin-up interaktif

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./style.css";
 
-function App() {
+const App = () => {
+  const [active, setActive] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`container ${active ? "active" : ""}`} id="container">
+      <div className="form-container sign-up">
+        <form>
+          <h1>Create Account</h1>
+          <div className="social-icons">
+            <button className="icons">
+              <i className="bx bxl-google"></i>
+            </button>
+            <button className="icons">
+              <i className="bx bxl-facebook"></i>
+            </button>
+            <button className="icons">
+              <i className="bx bxl-github"></i>
+            </button>
+            <button className="icons">
+              <i className="bx bxl-linkedin"></i>
+            </button>
+          </div>
+          <span>Register with E-mail</span>
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Enter E-mail" />
+          <input type="password" placeholder="Enter Password" />
+          <button>Sign Up</button>
+        </form>
+      </div>
+
+      <div className="form-container sign-in">
+        <form>
+          <h1>Sign In</h1>
+          <div className="social-icons">
+            <button className="icons">
+              <i className="bx bxl-google"></i>
+            </button>
+            <button className="icons">
+              <i className="bx bxl-facebook"></i>
+            </button>
+            <button className="icons">
+              <i className="bx bxl-github"></i>
+            </button>
+            <button className="icons">
+              <i className="bx bxl-linkedin"></i>
+            </button>
+          </div>
+          <span>Login With Email & Password</span>
+          <input type="email" placeholder="Enter E-mail" />
+          <input type="password" placeholder="Enter Password" />
+          <button className="forget-password-button">Forget Password?</button>
+          <button>Sign In</button>
+        </form>
+      </div>
+
+      <div className="toggle-container">
+        <div className="toggle">
+          <div className="toggle-panel toggle-left">
+            <h1>
+              Welcome To <br />
+              TechSolutions
+            </h1>
+            <p>Sign in With ID & Password</p>
+            <button
+              className="hidden"
+              id="login"
+              onClick={() => setActive(false)}
+            >
+              Sign In
+            </button>
+          </div>
+          <div className="toggle-panel toggle-right">
+            <h1>Hii Coder's</h1>
+            <p>Join "Code With TechSolutions" to Improve Your Coding Skills</p>
+            <button
+              className="hidden"
+              id="register"
+              onClick={() => setActive(true)}
+            >
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
